969c7adef62131b42b4bfb55bd00a6a68ff6bafe
Yusuf Kalın <seyfullahejderha0@gmail.com@example.com>
2026-03-06 10:27:14 +0300

    Fix fatal uncatchable iOS OpenGL SIGABRT caused by 0x0 screen dimensions generating a NaN projection matrix in ResourceManager initialization [WC-DIAG9], version 13

--- core/src/studioyes/kelimedunyasi/managers/ResourceManager.java
+++ core/src/studioyes/kelimedunyasi/managers/ResourceManager.java
@@ -10,76 +10,75 @@ import com.badlogic.gdx.utils.Disposable;
 @SuppressWarnings("unchecked")
 public class ResourceManager implements Disposable {
 
-    //atlas files
-    public static String ATLAS_1                    = "textures/atlas1.atlas";
-    public static String ATLAS_2                    = "textures/atlas2.atlas";
-    public static String ATLAS_3                    = "textures/atlas3.atlas";
-    public static String ATLAS_4                    = "textures/atlas4.atlas";
+    // atlas files
+    public static String ATLAS_1 = "textures/atlas1.atlas";
+    public static String ATLAS_2 = "textures/atlas2.atlas";
+    public static String ATLAS_3 = "textures/atlas3.atlas";
+    public static String ATLAS_4 = "textures/atlas4.atlas";
 
-    public static String ATLAS_5                    = "textures/atlas5.atlas";
+    public static String ATLAS_5 = "textures/atlas5.atlas";
 
-    //background images
+    // background images
     public static String introBackground, gameBackground;
 
-    //fonts
-    public static String fontSemiBold               = "fonts/noto_sans_ui.fnt";
-    public static String fontSemiBoldShadow         = "fonts/noto_sans_ui.fnt";
-    public static String fontBlack                  = "fonts/noto_sans_ui.fnt";
-    public static String fontBoardAndDialFont       = "fonts/noto_sans_ui.fnt";
+    // fonts
+    public static String fontSemiBold = "fonts/noto_sans_ui.fnt";
+    public static String fontSemiBoldShadow = "fonts/noto_sans_ui.fnt";
+    public static String fontBlack = "fonts/noto_sans_ui.fnt";
+    public static String fontBoardAndDialFont = "fonts/noto_sans_ui.fnt";
 
+    // sound effects
+    public static final String SFX_BLAST = "sfx/blast.mp3";
+    public static final String SFX_HINT = "sfx/hint.mp3";
+    public static final String SFX_MONSTER_JUMP = "sfx/monster_jump.mp3";
+    public static final String SFX_LEVEL_END = "sfx/level_end.mp3";
+    public static final String SFX_ROCKET = "sfx/rocket.mp3";
+    public static final String SFX_SELECT_1 = "sfx/SELECT_1.mp3";
+    public static final String SFX_SELECT_2 = "sfx/SELECT_2.mp3";
+    public static final String SFX_SELECT_3 = "sfx/SELECT_3.mp3";
+    public static final String SFX_SELECT_4 = "sfx/SELECT_4.mp3";
+    public static final String SFX_SELECT_5 = "sfx/SELECT_5.mp3";
+    public static final String SFX_SELECT_6 = "sfx/SELECT_6.mp3";
+    public static final String SFX_SELECT_7 = "sfx/SELECT_7.mp3";
+    public static final String SFX_SELECT_8 = "sfx/SELECT_8.mp3";
+    public static final String SFX_SPIN_CLICK = "sfx/spin_click.mp3";
+    public static final String SFX_SUCCESS = "sfx/success.mp3";
+    public static final String SFX_WHEEL_SPIN = "sfx/wheel_spin.mp3";
+    public static final String SFX_FOUND_BEFORE = "sfx/found_before.mp3";
+    public static final String SFX_BONUS_WORD = "sfx/bonus_word.mp3";
+    public static final String SFX_WRONG = "sfx/wrong.mp3";
+    public static final String SFX_SHUFFLE = "sfx/shuffle.mp3";
+    public static final String SFX_NOTIFICATION = "sfx/notification.mp3";
+    public static final String SFX_HIT_BOOSTER = "sfx/hit_booster.mp3";
 
-
-    //sound effects
-    public static final String SFX_BLAST            = "sfx/blast.mp3";
-    public static final String SFX_HINT             = "sfx/hint.mp3";
-    public static final String SFX_MONSTER_JUMP     = "sfx/monster_jump.mp3";
-    public static final String SFX_LEVEL_END        = "sfx/level_end.mp3";
-    public static final String SFX_ROCKET           = "sfx/rocket.mp3";
-    public static final String SFX_SELECT_1         = "sfx/SELECT_1.mp3";
-    public static final String SFX_SELECT_2         = "sfx/SELECT_2.mp3";
-    public static final String SFX_SELECT_3         = "sfx/SELECT_3.mp3";
-    public static final String SFX_SELECT_4         = "sfx/SELECT_4.mp3";
-    public static final String SFX_SELECT_5         = "sfx/SELECT_5.mp3";
-    public static final String SFX_SELECT_6         = "sfx/SELECT_6.mp3";
-    public static final String SFX_SELECT_7         = "sfx/SELECT_7.mp3";
-    public static final String SFX_SELECT_8         = "sfx/SELECT_8.mp3";
-    public static final String SFX_SPIN_CLICK       = "sfx/spin_click.mp3";
-    public static final String SFX_SUCCESS          = "sfx/success.mp3";
-    public static final String SFX_WHEEL_SPIN       = "sfx/wheel_spin.mp3";
-    public static final String SFX_FOUND_BEFORE     = "sfx/found_before.mp3";
-    public static final String SFX_BONUS_WORD       = "sfx/bonus_word.mp3";
-    public static final String SFX_WRONG            = "sfx/wrong.mp3";
-    public static final String SFX_SHUFFLE          = "sfx/shuffle.mp3";
-    public static final String SFX_NOTIFICATION     = "sfx/notification.mp3";
-    public static final String SFX_HIT_BOOSTER      = "sfx/hit_booster.mp3";
-
-    //shaders
-    public static final String SHADER_LINE          = "shader/line.frag";
-    public static final String SHADER_DIAL          = "shader/dial.fsh";
-    public static final String SHADER_OVERLAY       = "shader/overlay.fsh";
-    public static final String SHADER_VERTEX        = "shader/vertex.vsh";
-
-
-
+    // shaders
+    public static final String SHADER_LINE = "shader/line.frag";
+    public static final String SHADER_DIAL = "shader/dial.fsh";
+    public static final String SHADER_OVERLAY = "shader/overlay.fsh";
+    public static final String SHADER_VERTEX = "shader/vertex.vsh";
 
     /***** DO NOT EDIT ANYTHING BELOW *****/
 
     private final AssetManager manager = new AssetManager();
-
 
     public static String LOCALE_PROPERTIES_FILE;
 
     public static float scaleFactor = 1.0f;
     public static String resolvedRes;
 
-
-
-
-    public static void init(){
-        if(resolvedRes != null) return;
+    public static void init() {
+        if (resolvedRes != null)
+            return;
 
         float screenWidth = Gdx.graphics.getWidth();
         float screenHeight = Gdx.graphics.getHeight();
+
+        boolean isFallback = false;
+        if (screenWidth == 0 || screenHeight == 0) {
+            screenWidth = 320;
+            screenHeight = 480;
+            isFallback = true;
+        }
 
         float largeResolutionWidth = 1536;
         float largeResolutionHeight = 2048;
@@ -90,30 +89,28 @@ public class ResourceManager implements Disposable {
         float smallResolutionWidth = 320;
         float smallResolutionHeight = 480;
 
-
-        if(screenHeight > 1024){
+        if (screenHeight > 1024) {
             resolvedRes = "_hdr";
             scaleFactor = Math.min(screenHeight / largeResolutionHeight, screenWidth / largeResolutionWidth);
-        }else if(screenHeight > 480){
+        } else if (screenHeight > 480) {
             resolvedRes = "_hd";
             scaleFactor = Math.min(screenHeight / mediumResolutionHeight, screenWidth / mediumResolutionWidth);
-        }else {
+        } else {
             resolvedRes = "_sd";
             scaleFactor = Math.min(screenHeight / smallResolutionHeight, screenWidth / smallResolutionWidth);
         }
-        //Gdx.app.log("resolution", resolvedRes + ", " + scaleFactor);
+
+        if (isFallback) {
+            resolvedRes = null; // Forces re-initialization on next resize()
+        }
     }
 
+    public static String resolveResolutionAwarePath(String fileName) {
 
-
-
-
-    public static String resolveResolutionAwarePath(String fileName){
-
-        if(resolvedRes == null)
+        if (resolvedRes == null)
             init();
 
-        if(fileName.toLowerCase().contains(resolvedRes + ".")) {
+        if (fileName.toLowerCase().contains(resolvedRes + ".")) {
             return fileName;
         }
 
@@ -121,13 +118,11 @@ public class ResourceManager implements Disposable {
         return fileNameParts[0] + resolvedRes + "." + fileNameParts[1];
     }
 
-
-
-    public void load(String fileName, Class type){
+    public void load(String fileName, Class type) {
         manager.load(fileName, type);
     }
 
-    public void load(String fileName, Class type, AssetLoaderParameters parameter){
+    public void load(String fileName, Class type, AssetLoaderParameters parameter) {
         manager.load(fileName, type, parameter);
     }
 
@@ -140,54 +135,47 @@ public class ResourceManager implements Disposable {
         }
     }
 
-
-
-    public boolean update (int millis){
+    public boolean update(int millis) {
         return manager.update(millis);
     }
 
-    public synchronized float getProgress(){
+    public synchronized float getProgress() {
         return manager.getProgress();
     }
 
-
-    public synchronized boolean isFinished (){
+    public synchronized boolean isFinished() {
         return manager.isFinished();
     }
 
-    public synchronized <T> T get (String fileName, Class<T> type) {
+    public synchronized <T> T get(String fileName, Class<T> type) {
         return manager.get(fileName, type);
     }
 
-    public <T> Array<T> getAll(java.lang.Class<T> type,  Array<T> out){
+    public <T> Array<T> getAll(java.lang.Class<T> type, Array<T> out) {
         return manager.getAll(type, out);
     }
 
-
-    public void setLoader (Class type, AssetLoader loader){
+    public void setLoader(Class type, AssetLoader loader) {
         manager.setLoader(type, loader);
     }
 
-    public void finishLoading(){
+    public void finishLoading() {
         manager.finishLoading();
     }
 
-    public void finishLoadingAsset(String fileName){
+    public void finishLoadingAsset(String fileName) {
         manager.finishLoadingAsset(fileName);
     }
 
-
-    public boolean contains(String fileName){
+    public boolean contains(String fileName) {
         return manager.contains(fileName);
     }
 
-
-    public void unload(String fileName){
+    public void unload(String fileName) {
         manager.unload(fileName);
     }
 
-
-    public void clear(){
+    public void clear() {
         manager.clear();
     }
 
@@ -195,8 +183,5 @@ public class ResourceManager implements Disposable {
     public void dispose() {
         manager.dispose();
     }
-
-
-
 
 }
