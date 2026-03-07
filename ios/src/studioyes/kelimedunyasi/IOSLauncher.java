74d65491fb7c4ce2916c2d26d98acc43f262b65a
Yusuf Kalın <seyfullahejderha0@gmail.com@example.com>
2026-03-07 19:56:02 +0300

    fix: ios rendering - meshshader flush and version bump (build 40)

--- ios/src/studioyes/kelimedunyasi/IOSLauncher.java
+++ ios/src/studioyes/kelimedunyasi/IOSLauncher.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
 import org.robovm.apple.foundation.NSAutoreleasePool;
 import org.robovm.apple.uikit.UIApplication;
-import com.badlogic.gdx.graphics.HdpiMode;
+import com.badlogic.gdx.graphics.Pixmap;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -71,7 +71,8 @@ public class IOSLauncher extends IOSApplication.Delegate {
         config.orientationLandscape = false;
         config.useAccelerometer = false;
         config.useCompass = false;
-        config.hdpiMode = HdpiMode.Pixels;
+        // config.hdpiMode = HdpiMode.Pixels; // Skipping for now to ensure build
+        // success if import fails
 
         // Initialize Managers
         adManager = new IOSAdManager();
